e7b7491aa519af227a28d3f2f317d60b810c9147
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-11-07 21:23:34 +0200

    chore: Enable touchpad support and configure Steam and GameMode settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -123,7 +123,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mohamed = {
@@ -149,7 +149,6 @@
   # Allow unfree packages
   nixpkgs.config = {
     allowUnfree = true;
-    # packageOverrides = pkgs: { unstable = import unstableTarball { config = config.nixpkgs.config; }; };
   };
   #   environment.variables = {
   #     NIXPKGS_ALLOW_UNFREE = 1;
@@ -197,12 +196,12 @@
   #   programs.calls.enable = true;
   programs.appimage.enable = true;
   #   programs.adb.enable = true;
-  #   programs.gamemode.enable = true; # for performance mode
-  #   programs.steam = {
-  #     enable = true; # install steam
-  #     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #   };
+  programs.gamemode.enable = true; # for performance mode
+  programs.steam = {
+    enable = true; # install steam
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
   #   services.ollama = {
   #     enable = true;
   #     acceleration = "cuda";
@@ -268,30 +267,30 @@
   # Enable OpenGL
   hardware.graphics.enable = true;
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-    # Use the NVidia open source kernel module (not to be confused with the independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # hardware.nvidia = {
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   # Enable this if you have graphical corruption issues or application crashes after waking
+  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+  #   # of just the bare essentials.
+  #   powerManagement.enable = true;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
+  #   # Use the NVidia open source kernel module (not to be confused with the independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of supported GPUs is at:
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = false;
+  #   # Enable the Nvidia settings menu,
+  #   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
