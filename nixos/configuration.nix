e1ea57c6115cd22cbd7e22758cff7e2bd608f669
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-11-10 22:50:31 +0200

    chore: Update flake.lock and configuration.nix for locale settings and revision changes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,6 +56,11 @@
 
   i18n = {
     extraLocaleSettings = {
+      LANGUAGE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      LC_COLLATE = "en_US.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
       LC_ADDRESS = "en_US.UTF-8";
       LC_IDENTIFICATION = "en_US.UTF-8";
       LC_MEASUREMENT = "en_US.UTF-8";
