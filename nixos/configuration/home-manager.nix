d7561e632ce2e9e735825ad02c3c2d51b7145200
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-12-17 00:48:13 +0200

    Refactor NixOS configuration by reorganizing home-manager imports and creating separate laptop and desktop configurations

--- nixos/configuration/home-manager.nix
+++ nixos/configuration/home-manager.nix
@@ -3,7 +3,7 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.mohamed = import ./home.nix;
+    users.mohamed = import ./home-manager/home.nix;
     extraSpecialArgs = { };
     backupFileExtension =
       "backup-" + pkgs.lib.readFile "${pkgs.runCommand "timestamp" { } "echo -n `date '+%Y%m%d%H%M%S'` > $out"}";
