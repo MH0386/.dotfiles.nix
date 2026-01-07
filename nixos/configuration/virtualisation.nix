d040662819962b689873f43557ca9dba2cd44383
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-07 02:37:43 +0200

    Refactor virtualisation configuration: replace podman with docker and enable rootless mode

--- nixos/configuration/virtualisation.nix
+++ nixos/configuration/virtualisation.nix
@@ -7,16 +7,11 @@
       qemu.package = pkgs.qemu_kvm;
     };
     containers.enable = true;
-    podman = {
+    docker = {
       enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-      extraPackages = with pkgs; [
-        gvisor
-        gvproxy
-      ];
+      rootless = {
+        enable = true;
+      };
     };
   };
 }
