ef952c21675fc58b83e973b3f3ab6abe9e1609da
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-07 19:22:57 +0200

    Refactor systemPackages list and clean up commented code

--- nixos/configuration/environment.nix
+++ nixos/configuration/environment.nix
@@ -18,13 +18,23 @@
     localBinInPath = true;
     homeBinInPath = true;
     systemPackages =
-      (with pkgs; [
+      (with inputs; [
+        zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+        # flox.packages.${system}.default
+        fh.packages.${system}.default
+        # nixos-conf-editor.packages.${system}.nixos-conf-editor
+        # nix-software-center.packages.${system}.nix-software-center
+      ])
+      ++ (with pkgs; [
+        atuin
+        sbctl
         unzip
         zip
         wget
         nixfmt-rfc-style
         nixpkgs-fmt
         nixd
+        nil
         lshw-gui
         lshw
         nvtopPackages.nvidia
@@ -57,28 +67,8 @@
         xorg.libXdmcp
         xorg.libXtst
         glib
-        cudatoolkit
-      ])
-      # ++ (with pkgs.NUR.packages; [
-      #   MH0386.fvm
-      # ])
-      ++ (with pkgs.stablePackages; [
-        # gnome-extension-manager
-      ])
-      ++ (with pkgs.stablePackages.gst_all_1; [
-        gstreamer
-        gst-plugins-base
-        gst-plugins-good
-        gst-plugins-bad
-        gst-plugins-ugly
-        gst-libav
-      ])
-      ++ (with inputs; [
-        # flox.packages.${system}.default
-        # fh.packages.${system}.default
-        # nixos-conf-editor.packages.${system}.nixos-conf-editor
-        # nix-software-center.packages.${system}.nix-software-center
-        # inputs.zen-browser.packages.${system}.specific
+        gnome-extension-manager
+        # cudatoolkit
         # cudaPackages.cudatoolkit
         # cudaPackages.nccl
         # cudaPackages.cudnn
