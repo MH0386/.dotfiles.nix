5a1c0975b549c240c02833d00201982d89ff2edf
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-07 19:18:46 +0200

    Enable lanzaboote in boot configuration and adjust systemd-boot settings

--- nixos/configuration/boot.nix
+++ nixos/configuration/boot.nix
@@ -10,8 +10,13 @@
     # extraModulePackages = [ pkgs.linuxPackages.nvidia_x11 ];
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot.enable = lib.mkForce false;
       efi.canTouchEfiVariables = true;
     };
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };
+    initrd.systemd.enable = true;
   };
 }
