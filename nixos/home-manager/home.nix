5faf13741b03222cfe6061e0bee909402b5566af
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-09 01:14:30 +0200

    Add zed-editor configuration with extensions for enhanced functionality

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -252,6 +252,56 @@
       enable = true;
       enableMcpIntegration = true;
     };
+    zed-editor = {
+      enable = true;
+      extensions = [
+        "html"
+        "toml"
+        "dockerfile"
+        "git-firefly"
+        "github-theme"
+        "log"
+        "docker-compose"
+        "csv"
+        "github-dark-default"
+        "rainbow-csv"
+        "pylsp"
+        "fleet-themes"
+        "mcp-server-context7"
+        "basher"
+        "colored-zed-icons-theme"
+        "mcp-server-github"
+        "python-requirements"
+        "wakatime"
+        "mcp-server-sequential-thinking"
+        "python-snippets"
+        "cspell"
+        "autocorrect"
+        "caddyfile"
+        "code-stats"
+        "json5"
+        "tombi"
+        "github-activity-summarizer"
+        "mcp-server-byterover"
+        "markdownlint"
+        "github-actions"
+        "jq"
+        "jsonl"
+        "desktop"
+        "mcp-server-sonarqube"
+        "opencode"
+        "arch-mcp"
+        "nix"
+        "ini"
+        "nu"
+        "codeowners"
+        "editorconfig"
+        "ghostty"
+        "pyrefly"
+        "pytest-language-server"
+        "mcp-server-brave-search"
+      ];
+    };
   };
 
   home = {
