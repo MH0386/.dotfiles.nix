a70414d8728fac07f60901e3b60f2dc2034813cc
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-10 19:42:56 +0200

    Set Mohamed's shell to zsh

--- nixos/configuration/users.nix
+++ nixos/configuration/users.nix
@@ -2,7 +2,7 @@
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mohamed = {
-    shell = pkgs.xonsh;
+    shell = pkgs.zsh;
     isNormalUser = true;
     description = "Mohamed Hisham";
     extraGroups = [
