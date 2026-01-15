4e691a1ba769d1f522afc63399a73a89ab5d494b
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-15 14:08:45 +0200

    Enable GNOME extensions and update dconf settings

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -34,7 +34,6 @@
   };
 
   dconf.settings = {
-    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
     "org/gnome/desktop/default-applications".terminal = "ghostty.desktop";
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
@@ -57,6 +56,50 @@
       name = "Launch Clipboard History";
       command = "vicinae deeplink vicinae://extensions/vicinae/clipboard/history";
       binding = "<Super>v";
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      disabled-extensions = "disabled";
+      enabled-extensions = [
+        "dash-to-dock@micxgx.gmail.com"
+        "vicinae@dagimg-dot"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "caffeine@patapon.info"
+        "clipboard-indicator@tudmotu.com"
+        "gtk4-ding@smedius.gitlab.com"
+        "system-monitor@gnome-shell-extensions.gcampax.github.com"
+      ];
+      favorite-apps = [
+        "zen.desktop"
+        "org.gnome.Nautilus.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "pycharm.desktop"
+        "code.desktop"
+        "antigravity.desktop"
+        "dev.zed.Zed.desktop"
+      ];
+    };
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      always-center-icons = true;
+      click-action = "focus-minimize-or-appspread";
+      custom-theme-shrink = true;
+      dash-max-icon-size = 40;
+      dock-position = "BOTTOM";
+      extend-height = false;
+      intellihide-mode = "FOCUS_APPLICATION_WINDOWS";
+      scroll-action = "cycle-windows";
+      show-mounts-network= true;
+      transparency-mode = "DYNAMIC";
+    };
+    "org/gnome/desktop/interface" = {
+      clock-format ="12h";
+      clock-show-date = true;
+      clock-show-weekday = true;
+      color-scheme = "prefer-dark";
+      cursor-blink = true;
+      enable-animations = true;
+      enable-hot-corners = true;
+      show-battery-percentage = true;
     };
   };
 
