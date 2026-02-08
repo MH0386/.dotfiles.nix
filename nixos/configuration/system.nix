8b7d4c1b91c1174d4821ddc592fe2877916ff325
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-02-08 19:02:07 +0200

    Add `cache.nixos-cuda.org` cache

--- nixos/configuration/system.nix
+++ nixos/configuration/system.nix
@@ -11,12 +11,14 @@
       "https://nix-community.cachix.org"
       "https://devenv.cachix.org"
       "https://cache.flox.dev"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
     trusted-users = [
       "root"
