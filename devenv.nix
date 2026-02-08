44835c567242d180c01383e91c0368a701a3fa63
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-02-08 09:56:25 +0200

    Add initial configuration files for devenv setup

---
+++ devenv.nix
@@ -0,0 +1,73 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  languages = {
+    nix = {
+      enable = true;
+      lsp.enable = true;
+    };
+  };
+
+  git-hooks.hooks = {
+    action-validator.enable = true;
+    actionlint.enable = true;
+    nixfmt.enable = true;
+    check-added-large-files.enable = true;
+    check-case-conflicts.enable = true;
+    check-json.enable = true;
+    check-merge-conflicts.enable = true;
+    check-toml.enable = true;
+    check-vcs-permalinks.enable = true;
+    check-xml.enable = true;
+    check-yaml.enable = true;
+    comrak.enable = true;
+    deadnix.enable = true;
+    detect-private-keys.enable = true;
+    markdownlint.enable = true;
+    mixed-line-endings.enable = true;
+    yamlfmt.enable = true;
+    ripsecrets.enable = true;
+    statix.enable = true;
+    taplo.enable = true;
+    trim-trailing-whitespace.enable = true;
+    trufflehog.enable = true;
+    yamllint.enable = true;
+  };
+
+  treefmt = {
+    enable = true;
+    config = {
+      programs = {
+        actionlint.enable = true;
+        jsonfmt.enable = true;
+        nixf-diagnose.enable = true;
+        nixfmt.enable = true;
+        deadnix.enable = true;
+        oxipng.enable = true;
+        statix.enable = true;
+        taplo.enable = true;
+        xmllint.enable = true;
+        yamlfmt.enable = true;
+      };
+      settings = {
+        formatter = {
+          taplo-format = {
+            command = "${lib.getExe pkgs.taplo}";
+            options = [ "format" ];
+            includes = [ "*.toml" ];
+            excludes = [
+              ".git/*"
+              ".devenv/*"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
