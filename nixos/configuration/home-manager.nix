2f2400e9035997c4eba9d53c2a0764acb88c2498
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-02-09 17:25:24 +0200

    fix(nixos): add pkgsStable to extraSpecialArgs in home-manager configuration

--- nixos/configuration/home-manager.nix
+++ nixos/configuration/home-manager.nix
@@ -1,4 +1,4 @@
-{ pkgs, nur, ... }:
+{ pkgs, nur,  pkgsStable, ...}:
 let
   timestamp = pkgs.lib.readFile "${pkgs.runCommand "timestamp" { }
     "echo -n `date '+%Y%m%d%H%M%S'` > $out"
@@ -9,7 +9,10 @@ in
     useGlobalPkgs = true;
     useUserPackages = true;
     users.mohamed = import ./../home-manager/hm-configuration.nix;
-    extraSpecialArgs = { inherit nur; };
+    extraSpecialArgs = {
+      inherit nur;
+      inherit pkgsStable;
+    };
     backupFileExtension = "backup-${timestamp}";
     verbose = true;
   };
