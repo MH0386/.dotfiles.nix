1856cbda2e74c8b778cea20d47755cb0f693596f
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-03 00:53:49 +0200

    Add configuration modules for dank-material-shell and niri window managers

---
+++ modules/config/window_manager/niri.nix
@@ -0,0 +1,26 @@
+{ delib, ... }:
+delib.module {
+  name = "niri";
+
+  options.niri = with delib; {
+    enable = boolOption host.niriFeatured;
+    package = packageOption pkgs.niri;
+    iio-niri.enable = boolOption true;
+  };
+
+  nixos.ifEnabled =
+    { cfg, ... }:
+    {
+      programs.niri.enable = cfg.enable;
+      services.iio-niri.enable = cfg.iio-niri.enable;
+    };
+
+  home.ifEnabled =
+    { cfg, ... }:
+    {
+      programs.niri = {
+        inherit (cfg) enable;
+        inherit (cfg) package;
+      };
+    };
+}
