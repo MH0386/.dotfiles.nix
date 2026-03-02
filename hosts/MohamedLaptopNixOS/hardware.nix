1caaabbe01843693db4dd0a0606a470a40640f55
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-03 00:39:34 +0200

    Add configuration for MohamedLaptopNixOS host

---
+++ hosts/MohamedLaptopNixOS/hardware.nix
@@ -0,0 +1,20 @@
+{
+  delib,
+  ...
+}:
+let
+  systemArch = "x86_64-linux";
+  stateVersion = "26.05";
+in
+delib.host {
+  name = "MohamedLaptopNixOS";
+
+  homeManagerSystem = systemArch;
+  home.home.stateVersion = stateVersion;
+
+  nixos = {
+    nixpkgs.hostPlatform = systemArch;
+    system.stateVersion = stateVersion;
+    imports = [ ./hardware-configuration.nix ];
+  };
+}
