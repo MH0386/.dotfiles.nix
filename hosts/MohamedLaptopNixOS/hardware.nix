50a3f9e313aa7962f8374a9937460c6ec8380cdc
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-05 17:19:06 +0200

    Update hardware.nix

--- hosts/MohamedLaptopNixOS/hardware.nix
+++ hosts/MohamedLaptopNixOS/hardware.nix
@@ -9,6 +9,7 @@ in
 delib.host {
   name = "MohamedLaptopNixOS";
 
+  # myconfig.boot.mode = "uefi";
   system = systemArch;
   homeManagerSystem = systemArch;
   home.home.stateVersion = stateVersion;
