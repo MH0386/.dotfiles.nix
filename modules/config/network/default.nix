13bc0abd41c7f1b999578ec5ec4e767c1f71be94
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-05 17:38:05 +0200

    Add hostname and networkmanager group
    Set the hostname based on the host configuration. Add the user to the
    "networkmanager" group to allow it to manage network connections.

--- modules/config/network/default.nix
+++ modules/config/network/default.nix
@@ -1,11 +1,21 @@
-{ delib, ... }:
+{ delib, host, ... }:
 delib.module {
   name = "network";
 
-  nixos.always.networking = {
-    # Enable networking
-    networkmanager.enable = true;
-    # Enables wireless support via wpa_supplicant.
-    wireless.enable = true;
-  };
+  nixos.always =
+    { myconfig, ... }:
+    let
+      inherit (myconfig.constants) username;
+    in
+    {
+      networking = {
+        hostName = host.name;
+        # Enable networking
+        networkmanager.enable = true;
+        # Enables wireless support via wpa_supplicant.
+        wireless.enable = true;
+      };
+      users.users.${username}.extraGroups = [ "networkmanager" ];
+
+    };
 }
