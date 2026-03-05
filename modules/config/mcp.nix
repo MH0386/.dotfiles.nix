21316ec3316f3932bbef922cbfc1a36b02013543
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-06 00:15:03 +0200

    Use hosted devenv mcp

--- modules/config/mcp.nix
+++ modules/config/mcp.nix
@@ -23,10 +23,7 @@ delib.module {
             url = "https://docs.agno.com/mcp";
           };
           devenv = {
-            command = "devenv";
-            args = [
-              "mcp"
-            ];
+            url = "mcp.devenv.sh";
           };
           git = {
             command = "uvx";
