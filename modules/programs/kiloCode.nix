95b4a97559302c47ab9bb4406445be15bdaec00c
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-05 18:29:34 +0200

    Add kiloCode module

---
+++ modules/programs/kiloCode.nix
@@ -0,0 +1,18 @@
+{
+  delib,
+  inputs,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "kiloCode";
+
+  options.kiloCode = with delib; {
+    enable = boolOption true;
+  };
+
+  home.ifEnabled.home.packages = [
+    inputs.kilocode.packages.${pkgs.stdenv.hostPlatform.system}.kilo
+    inputs.kilocode.packages.${pkgs.stdenv.hostPlatform.system}.desktop
+  ];
+}
