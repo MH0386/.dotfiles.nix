8f16911889d16ec22b1b24dede6810d18429d39e
mohamed <mohamed.hisham.abdelzaher@gmail.com>
2026-03-07 23:03:51 +0200

    Update boot.nix

--- modules/config/boot.nix
+++ modules/config/boot.nix
@@ -8,69 +8,34 @@
 delib.module {
   name = "boot";
 
-  options =
-    { cfg, ... }:
-    {
-      boot = with delib; {
-        loader = enumOption [ "grub" "systemd-boot" "lanzaboote" ] (
-          if cfg.mode == "uefi" then "lanzaboote" else "grub"
-        );
-
-        mode = enumOption [ "uefi" "legacy" ] (
-          if builtins.pathExists /sys/firmware/efi/efivars then "uefi" else "legacy"
-        );
-
-        plymouth = {
-          enable = boolOption true;
-        };
+  nixos.always = {
+    imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
+    boot = {
+      loader = {
+        efi.canTouchEfiVariables = true;
+        systemd-boot.enable = lib.mkForce false;
       };
-    };
-
-  nixos.always =
-    { cfg, ... }:
-    {
-      imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
-      boot = {
-        loader = {
-          efi.canTouchEfiVariables = true;
-
-          grub = lib.mkIf (cfg.loader == "grub") {
-            enable = true;
-            efiSupport = cfg.mode == "uefi";
-            devices = [ "nodev" ];
-            configurationLimit = 10;
-          };
-
-          systemd-boot = lib.mkMerge [
-            { enable = lib.mkForce cfg.loader == "systemd-boot"; }
-            (lib.mkIf (cfg.loader == "systemd-boot") {
-              configurationLimit = 10;
-              consoleMode = "max";
-            })
-          ];
-        };
-
-        lanzaboote = lib.mkIf (cfg.loader == "lanzaboote") {
-          enable = true;
-          pkiBundle = "/var/lib/sbctl";
-        };
-        kernelPackages = pkgs.linuxPackages_latest;
-        initrd.systemd.enable = true;
-        consoleLogLevel = 0;
-        initrd.verbose = false;
-        plymouth = {
-          inherit (cfg.plymouth) enable;
-          logo = "${pkgs.nixos-icons}/share/icons/hicolor/256x256/apps/nix-snowflake-white.png";
-          theme = "breeze";
-        };
-        kernelParams = [
-          "quiet"
-          "splash"
-          "rd.udev.log_level=3"
-          "udev.log_priority=3"
-          "boot.shell_on_fail"
-        ];
+      lanzaboote = {
+        enable = true;
+        pkiBundle = "/var/lib/sbctl";
       };
-      environment.systemPackages = lib.mkIf (cfg.loader == "lanzaboote") [ pkgs.sbctl ];
+      kernelPackages = pkgs.linuxPackages_latest;
+      initrd.systemd.enable = true;
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      plymouth = {
+        enable = true;
+        logo = "${pkgs.nixos-icons}/share/icons/hicolor/256x256/apps/nix-snowflake-white.png";
+        theme = "breeze";
+      };
+      kernelParams = [
+        "quiet"
+        "splash"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+        "boot.shell_on_fail"
+      ];
     };
+    environment.systemPackages = [ pkgs.sbctl ];
+  };
 }
