ff675051b9e2dcf776b082effa6dd7bb4aeb12a2
mohamed <mohamed.hisham.abdelzaher@gmail.com>
2026-03-07 23:04:28 +0200

    Update default.nix

--- modules/config/default.nix
+++ modules/config/default.nix
@@ -11,25 +11,25 @@ delib.module {
     environment = {
       localBinInPath = true;
       homeBinInPath = true;
-      systemPackages =
-        (with pkgs; [
-          gnumake
-          ntfs3g
-          gcc
-          clang
-          cmake
-          glib
-          unzip
-          zip
-          wget
-          lshw-gui
-          ignition
-        ])
-        ++ (with pkgsStable; [ fh ]);
     };
   };
 
   home.always.home = {
+    packages =
+      (with pkgs; [
+        gnumake
+        ntfs3g
+        gcc
+        cmake
+        unzip
+        zip
+        wget
+        lshw-gui
+        ignition
+      ])
+      ++ (with pkgsStable; [
+        fh
+      ]);
     file = { };
     sessionPath = [ ];
     sessionVariables = { };
