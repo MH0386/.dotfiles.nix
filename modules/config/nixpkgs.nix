171b7e1d7a6302e384f7e320db82e0c3b9a8c718
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-03-28 13:35:44 +0200

    Use Lix

--- modules/config/nixpkgs.nix
+++ modules/config/nixpkgs.nix
@@ -1,6 +1,18 @@
 {
   den.schema.conf = {
-    nixos.nixpkgs.config.allowUnfree = true;
+    nixos.nixpkgs = {
+      config.allowUnfree = true;
+      overlays = [
+        (final: prev: {
+          inherit (prev.lixPackageSets.stable)
+            nixpkgs-review
+            nix-eval-jobs
+            nix-fast-build
+            colmena
+            ;
+        })
+      ];
+    };
     homeManager.nixpkgs.config.allowUnfree = true;
   };
 }
