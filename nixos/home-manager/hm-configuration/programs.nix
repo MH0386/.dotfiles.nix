46cc02501593306dc0915d4ad6c627b2e2348817
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-05 17:21:24 +0200

    Add shell integrations for direnv

--- nixos/home-manager/hm-configuration/programs.nix
+++ nixos/home-manager/hm-configuration/programs.nix
@@ -210,6 +210,10 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+      enableNushellIntegration = true;
     };
     vicinae = {
       enable = true;
