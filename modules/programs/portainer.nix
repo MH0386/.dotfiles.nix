084a599e6955ab63940549708c22ff8cac299b02
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-06 21:49:08 +0200

    Fix portainer.nix

--- modules/programs/portainer.nix
+++ modules/programs/portainer.nix
@@ -1,15 +1,14 @@
+{ inputs, ... }:
 {
   den.default = {
-    nixos =
-      { inputs, ... }:
-      {
-        imports = [ inputs.portainer-on-nixos.nixosModules.portainer ];
-        services.portainer = {
-          enable = true;
-          version = "latest";
-          openFirewall = true;
-          port = 9443;
-        };
+    nixos = {
+      imports = [ inputs.portainer-on-nixos.nixosModules.portainer ];
+      services.portainer = {
+        enable = true;
+        version = "latest";
+        openFirewall = true;
+        port = 9443;
       };
+    };
   };
 }
