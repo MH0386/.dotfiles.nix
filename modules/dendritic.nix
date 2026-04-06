aca658e754e06c0adcfae498dbbb1068f39b02b3
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-06 21:23:29 +0200

    Update dendritic.nix

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -7,9 +7,55 @@
   flake-file.inputs = {
     den.url = "github:vic/den";
     flake-file.url = "github:vic/flake-file";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    # nix-software-center = {
+    #   url = "github:ljubitje/nix-software-center";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # nixos-conf-editor = {
+    #   url = "github:snowfallorg/nixos-conf-editor";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    portainer-on-nixos = {
+      url = "gitlab:cbleslie/portainer-on-nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-jetbrains-plugins = {
+      url = "github:nix-community/nix-jetbrains-plugins";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dms-plugin-registry = {
+      url = "github:AvengeMedia/dms-plugin-registry";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    oh-my-bash = {
+      url = "github:ohmybash/oh-my-bash";
+      flake = false;
     };
   };
 }
