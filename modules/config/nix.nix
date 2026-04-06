10d76b82004f3c5bc677d7831311a75c6ba826c3
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-06 21:27:00 +0200

    Update nix.nix

--- modules/config/nix.nix
+++ modules/config/nix.nix
@@ -1,44 +1,33 @@
 {
   den.default = {
     nixos = {
-        nix = {
-          package = pkgs.lixPackageSets.stable.lix;
-          settings = {
-            experimental-features = [
-              "flakes"
-              "nix-command"
-            ];
-            substituters = [
-              "https://cuda-maintainers.cachix.org"
-              "https://nix-community.cachix.org"
-              "https://devenv.cachix.org"
-              "https://cache.flox.dev"
-              "https://cache.nixos-cuda.org"
-              "https://cache.garnix.io"
-            ];
-            trusted-public-keys = [
-              "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-              "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-              "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-              "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-            ];
-            trusted-users = [
-              "root"
-              "mohamed"
-            ];
-          };
-        };
-        programs.nix-ld.enable = true;
-        environment.systemPackages = with pkgs; [
-          nix-tree
-          nixpkgs-reviewFull
-          nixfmt
-          nixd
-          nil
-          nix-diff
+      nix.settings = {
+        experimental-features = [
+          "flakes"
+          "nix-command"
+        ];
+        substituters = [
+          "https://cuda-maintainers.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://devenv.cachix.org"
+          "https://cache.flox.dev"
+          "https://cache.nixos-cuda.org"
+          "https://cache.garnix.io"
+        ];
+        trusted-public-keys = [
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        ];
+        trusted-users = [
+          "root"
+          "mohamed"
         ];
       };
+      programs.nix-ld.enable = true;
+    };
   };
 }
