c8c488dd055c41469c6ed7132770c58dd610ea4e
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-08 14:21:31 +0200

    Update displayManager.nix

--- modules/config/displayManager.nix
+++ modules/config/displayManager.nix
@@ -1,29 +1,26 @@
 {
-  den.default = {
+  den.provides = {
+    gdm = {
+      nixos.services.displayManager.gdm.enable = true;
+      homeManager =
+        { pkgs, ... }:
+        {
+          home.packages = [ pkgs.gdm-settings ];
+        };
+    };
+    dmsGreeter = {
+      nixos.services.displayManager.dms-greeter = {
+        enable = true;
+        compositor.name = "niri";
 
-    provides = {
-      gdm = {
-        nixos.services.displayManager.gdm.enable = true;
-        homeManager =
-          { pkgs, ... }:
-          {
-            home.packages = [ pkgs.gdm-settings ];
-          };
-      };
-      dmsGreeter = {
-        nixos.services.displayManager.dms-greeter = {
-          enable = true;
-          compositor.name = "niri";
+        # Sync your user's DankMaterialShell theme with the greeter.
+        # You'll probably want this
+        configHome = "/home/mohamed";
 
-          # Sync your user's DankMaterialShell theme with the greeter.
-          # You'll probably want this
-          configHome = "/home/mohamed";
-
-          # Save the logs to a file
-          logs = {
-            save = true;
-            path = "/tmp/dms-greeter.log";
-          };
+        # Save the logs to a file
+        logs = {
+          save = true;
+          path = "/tmp/dms-greeter.log";
         };
       };
     };
