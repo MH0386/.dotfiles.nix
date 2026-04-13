cdce7aa04aaf29d43bb45a586f40e5b9d22c11ae
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-13 18:02:43 +0200

    Update printing.nix

--- modules/config/printing.nix
+++ modules/config/printing.nix
@@ -1,7 +1,29 @@
 {
   den.default = {
     # Enable CUPS to print documents.
-
     nixos.services.printing.enable = true;
+  };
+
+  den.aspects.MohamedDesktopNixOS = {
+    nixos =
+      { pkgsStable, ... }:
+      {
+        # Enable CUPS to print documents.
+        services.printing = {
+          listenAddresses = [ "*:631" ];
+          allowFrom = [ "all" ];
+          browsing = true;
+          defaultShared = true;
+          openFirewall = true;
+          drivers = [ pkgsStable.hplipWithPlugin ];
+        };
+        # Enable SANE scanning support.
+        hardware.sane = {
+          enable = false;
+          extraBackends = [ pkgsStable.hplipWithPlugin ];
+          openFirewall = true;
+          backends-package = pkgsStable.sane-backends;
+        };
+      };
   };
 }
