5ba155d96b41d9fc7f666943f4693c354cd1c43c
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-13 19:25:28 +0200

    Update nvidia.nix

--- modules/config/nvidia.nix
+++ modules/config/nvidia.nix
@@ -86,6 +86,14 @@
       };
       MohamedLaptopNixOS = {
         nixpkgs.config.cudaCapabilities = [ "7.5" ];
+        hardware.nvidia.prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
       };
     };
   };
