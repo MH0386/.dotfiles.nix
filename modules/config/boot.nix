83bb27fd5b555284f561617d1fe37a69ff62ee75
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-13 17:50:20 +0200

    Remove plymouth

--- modules/config/boot.nix
+++ modules/config/boot.nix
@@ -16,11 +16,6 @@
           initrd.systemd.enable = true;
           consoleLogLevel = 0;
           initrd.verbose = true;
-          plymouth = {
-            enable = true;
-            logo = "${pkgs.nixos-icons}/share/icons/hicolor/256x256/apps/nix-snowflake-white.png";
-            theme = "breeze";
-          };
           kernelParams = [
             "quiet"
             "splash"
