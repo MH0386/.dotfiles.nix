5d30347c733b9ddfe042b83e668109d8cfb2af4c
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-18 23:39:27 +0200

    Update mohamed.nix

--- modules/mohamed.nix
+++ modules/mohamed.nix
@@ -6,34 +6,6 @@
       den.provides.primary-user
       (den.provides.user-shell "zsh")
     ];
-    homeManager =
-      { pkgs, ... }:
-      {
-        home.packages = [
-          pkgs.htop
-          pkgs.vim
-        ];
-      };
-
-    nixos = {
-      # networking.hostName = "MohamedLaptopNixOS";
-      # boot.loader.grub.devices = [ "nodev" ];
-      boot.blacklistedKernelModules = [
-        "nouveau"
-        "nvidia_drm"
-        "nvidia_modeset"
-        "nvidia"
-      ];
-      hardware.nvidia.prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-      nixpkgs.config.cudaCapabilities = [ "7.5" ];
-    };
   };
   den.homes.x86_64-linux.mohamed = { };
 }
