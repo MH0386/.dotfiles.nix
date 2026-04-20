596b1f971d7dbb6cd2acde5becd16458e63c0dea
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-20 14:51:22 +0200

    Add mcps and settings to gemini cli

--- modules/programs/ai/gemini_cli.nix
+++ modules/programs/ai/gemini_cli.nix
@@ -3,6 +3,18 @@
     homeManager = {
       programs.gemini-cli = {
         enable = true;
+        enableMcpIntegration = true;
+        settings = {
+          security = {
+            auth = {
+              selectedType = "oauth-personal";
+            };
+          };
+          ide = {
+            hasSeenNudge = true;
+            enabled = true;
+          };
+        };
       };
     };
   };
