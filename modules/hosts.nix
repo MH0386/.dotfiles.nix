d08116174b131742c08cc74456e77fbddf1328ac
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-21 14:29:08 +0200

    Update hosts.nix

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,6 +1,19 @@
+{ den, ... }:
 {
-  den.hosts.x86_64-linux = {
-    MohamedLaptopNixOS.users.mohamed = { };
-    MohamedDesktopNixOS.users.mohamed = { };
+  den = {
+    hosts.x86_64-linux = {
+      MohamedLaptopNixOS.users.mohamed = { };
+      MohamedDesktopNixOS.users.mohamed = { };
+    };
+    aspects = {
+      MohamedLaptopNixOS = {
+        includes = [ den.provides.hostname ];
+        nixos.imports = [ ./hosts/MohamedLaptopNixOS/_hardware-configuration.nix ];
+      };
+      MohamedDesktopNixOS = {
+        includes = [ den.provides.hostname ];
+        nixos.imports = [ ./hosts/MohamedDesktopNixOS/_hardware-configuration.nix ];
+      };
+    };
   };
 }
