6898d56af61fd4f7c610c4f30af945eb3ac16148
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-21 15:03:40 +0200

    Import modules in its related module

--- modules/config/windowManager/niri/defaults.nix
+++ modules/config/windowManager/niri/defaults.nix
@@ -1,9 +1,13 @@
+{ inputs, ... }:
 {
   den.default = {
     nixos = {
       programs.niri.enable = true;
       services.iio-niri.enable = true;
     };
-    homeManager.programs.niri.enable = true;
+    homeManager = {
+      imports = [ inputs.niri.homeModules.niri ];
+      programs.niri.enable = true;
+    };
   };
 }
