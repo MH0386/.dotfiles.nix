5b9ea3e2e99364b28c50bf50d6b2d671c3a85a1b
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-25 01:10:11 +0300

    Update topgrade.nix

--- modules/programs/topgrade.nix
+++ modules/programs/topgrade.nix
@@ -2,6 +2,12 @@
   den.default = {
     homeManager.programs.topgrade = {
       enable = true;
+      settings = {
+        misc = {
+          assume_yes = true;
+          disable = [ "nix" ];
+        };
+      };
     };
   };
 }
