1dffc5552844b9b16afff0b9bb3ad18506c2e704
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-25 01:10:23 +0300

    Update warp_terminal.nix

--- modules/programs/terminal/warp_terminal.nix
+++ modules/programs/terminal/warp_terminal.nix
@@ -4,6 +4,9 @@
       { pkgs, ... }:
       {
         home.packages = [ pkgs.warp-terminal ];
+        home.sessionVariables = {
+          WARP_ENABLE_WAYLAND = "1";
+        };
       };
   };
 }
