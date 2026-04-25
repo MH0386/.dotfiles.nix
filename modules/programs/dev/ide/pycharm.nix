ee3a9f46cb7f0ec184108e205021b55d79d9866d
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-25 16:10:33 +0300

    Update pycharm.nix

--- modules/programs/dev/ide/pycharm.nix
+++ modules/programs/dev/ide/pycharm.nix
@@ -5,26 +5,25 @@
       { pkgs, ... }:
       {
         home.packages = [
-          (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.pycharm [
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."org.jetbrains.junie"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.intellij.ml.llm"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."mobi.hsz.idea.gitignore"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."nix-idea"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.github.xepozz.gitattributes"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.aquasecurity.plugins.intellij-Trivy"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."net.seesharpsoft.intellij.plugins.csv"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.intellij.ideolog"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."org.sonarlint.idea"
-            # inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."sourcery.pycharm-plugin"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."String Manipulation"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."tanvd.grazi"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."izhangzhihao.rainbow.brackets"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."indent-rainbow.indent-rainbow"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.ultrahob.zerolength.plugin"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.wakatime.intellij.plugin"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."ru.adelf.idea.dotenv"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.github.copilot"
-            inputs.nix-jetbrains-plugins.plugins.${pkgs.stdenv.hostPlatform.system}.pycharm."2025.3"."com.kozhun.commit-message-template"
+          (inputs.nix-jetbrains-plugins.lib.buildIdeWithPlugins pkgs "pycharm" [
+            "org.jetbrains.junie"
+            "com.intellij.ml.llm"
+            "mobi.hsz.idea.gitignore"
+            "nix-idea"
+            "com.github.xepozz.gitattributes"
+            "com.aquasecurity.plugins.intellij-Trivy"
+            "net.seesharpsoft.intellij.plugins.csv"
+            "com.intellij.ideolog"
+            "org.sonarlint.idea"
+            # "sourcery.pycharm-plugin"
+            "String Manipulation"
+            "tanvd.grazi"
+            "izhangzhihao.rainbow.brackets"
+            "indent-rainbow.indent-rainbow"
+            "com.ultrahob.zerolength.plugin"
+            "com.wakatime.intellij.plugin"
+            "ru.adelf.idea.dotenv"
+            "com.kozhun.commit-message-template"
           ])
         ];
       };
