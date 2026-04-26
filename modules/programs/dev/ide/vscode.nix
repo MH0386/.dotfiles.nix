31e2f2d8ab9fa678818f22d599a4e49726a0c9d7
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-04-27 01:19:43 +0300

    Update vscode.nix

--- modules/programs/dev/ide/vscode.nix
+++ modules/programs/dev/ide/vscode.nix
@@ -1,25 +1,20 @@
 {
   den.default = {
-    homeManager =
-      { config, lib, ... }:
-      {
-        home.sessionVariables = {
-          EDITOR = "${lib.getExe config.programs.vscode.package} --wait";
-        };
-        programs.vscode = {
-          enable = true;
-          profiles = {
-            default = {
-              enableMcpIntegration = true;
-              # userSettings = {
-              #   editor = {
-              #     fontSize = 14;
-              #     fontFamily = "Fira Code";
-              #   };
-              # };
-            };
+    homeManager = {
+      programs.vscode = {
+        enable = true;
+        profiles = {
+          default = {
+            enableMcpIntegration = true;
+            # userSettings = {
+            #   editor = {
+            #     fontSize = 14;
+            #     fontFamily = "Fira Code";
+            #   };
+            # };
           };
         };
       };
+    };
   };
 }
