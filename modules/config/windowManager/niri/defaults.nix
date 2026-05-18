d840098da4a0ae263865634726000e31fabbbf18
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-05-18 16:36:05 +0300

    feat: Add Niri window manager keybindings
    Update Niri configuration to include new keybindings for toggling
    vicinae,
    launching noctalia-shell commands for settings, session menu, lock
    screen,
    volume control, and brightness adjustments.

--- modules/config/windowManager/niri/defaults.nix
+++ modules/config/windowManager/niri/defaults.nix
@@ -21,13 +21,69 @@
           package = pkgs.niri;
           settings = {
             binds = {
-              "Alt+Space" = {
+              "Mod+Space" = {
                 action.spawn = [
                   "vicinae"
                   "toggle"
                 ];
                 repeat = false;
               };
+              "Mod+S".action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "settings"
+                "toggle"
+              ];
+              "Mod+L".action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "sessionMenu"
+                "toggle"
+              ];
+              "Mod+Shift+L".action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "lockScreen"
+                "lock"
+              ];
+              XF86AudioRaiseVolume.action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "volume"
+                "increase"
+              ];
+              XF86AudioLowerVolume.action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "volume"
+                "decrease"
+              ];
+              XF86AudioMute.action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "volume"
+                "muteOutput"
+              ];
+              XF86MonBrightnessUp.action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "brightness"
+                "increase"
+              ];
+              XF86MonBrightnessDown.action.spawn = [
+                "noctalia-shell"
+                "ipc"
+                "call"
+                "brightness"
+                "decrease"
+              ];
             };
             spawn-at-startup = [
               {
